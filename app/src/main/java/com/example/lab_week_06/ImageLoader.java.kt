703f1a8e6b1c42e85eb44148e4cacb09b8675abb
seronnnn <163845781+seronnnn@users.noreply.github.com>
2025-10-06 21:28:25 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_06/ImageLoader.java.kt
@@ -0,0 +1,6 @@
+package com.example.lab_week_06
+
+import android.widget.ImageView
+interface ImageLoader {
+    fun loadImage(imageUrl: String, imageView: ImageView)
+}
---
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.lab_week_06
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.lab_week_06.GlideImageLoader
+import com.example.lab_week_06.CatAdapter
+import com.example.lab_week_06.model.CatModel
+import com.example.lab_week_06.model.CatBreed
+import com.example.lab_week_06.model.Gender
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var catAdapter: CatAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler_view)
+        catAdapter = CatAdapter(layoutInflater, GlideImageLoader(this))
+
+        recyclerView.adapter = catAdapter
+        recyclerView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+        catAdapter.setData(
+            listOf(
+                CatModel(
+                    Gender.Male,
+                    CatBreed.BalineseJavanese,
+                    "Fred",
+                    "Silent and deadly",
+                    "https://cdn2.thecatapi.com/images/7dj.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.ExoticShorthair,
+                    "Wilma",
+                    "Cuddly assassin",
+                    "https://cdn2.thecatapi.com/images/egv.jpg"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.AmericanCurl,
+                    "Curious George",
+                    "Award winning investigator",
+                    "https://cdn2.thecatapi.com/images/bar.jpg"
+                )
+            )
+        )
+    }
+}
